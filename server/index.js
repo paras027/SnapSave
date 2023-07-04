0d2f5400620d187481054eb1d10cc79b967b2eb3
paras027 <89771891+paras027@users.noreply.github.com>
2023-07-04 12:50:44 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.js';
 
+import postRoutes from './routes/posts.js';
+import { } from 'dotenv/config'
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -15,7 +16,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Paras:Paras123@post.pnhgt2r.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT);
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
